ef591761ad8d83fd0d8ee09671303a54405c5d7e
THABATA GUERREIRO NASCIMENTO DE MORAES <thabata.gnmoraes@senacsp.edu.br>
2020-02-29 14:54:08 -0300

    Criação do código de alerta e a chamada no html

---
+++ aula02/js/script.js
@@ -0,0 +1,4 @@
+//Vamos criar uma janela de alerta para saudar os usuários!
+alert("Olá, seja bem vindo!");
+//alert - tem um cubo. Cubo = função, tem que ser colocado um parenteses
+//Dentro do alerta, tem uma mensagem que é feita a partir de uma String - coloque aspas para valer a pena!
