b6c9875d0a40bc5b7c09cffcee22913f54cd7462
THABATA GUERREIRO NASCIMENTO DE MORAES <thabata.gnmoraes@senacsp.edu.br>
2020-02-29 15:33:22 -0300

    Adição de linhas de código de prompt, e exibição do nome do usuário na tela

--- aula02/js/script.js
+++ aula02/js/script.js
@@ -2,3 +2,28 @@
 alert("Olá, seja bem vindo!");
 //alert - tem um cubo. Cubo = função, tem que ser colocado um parenteses
 //Dentro do alerta, tem uma mensagem que é feita a partir de uma String - coloque aspas para valer a pena!
+
+/*
+Vamos pedir ao usuario que digite o seu nome e depois mostraremos em uma página de alerta...
+Para executar a operação, iremos guardar o nome do usuário numa variável.
+A variável terá o nome de 'nome'
+*/
+
+var nome = prompt("Digite o seu nome: ", "Escreva o seu nome aqui"); //nome do texto + placeholder
+alert(nome);
+/*
+Para apresentar o nome do usuário na página HTML, foi necessário criar algum elemento de "link" (relação da página HTML com o Javascript).
+Para o vínculo, usamos o comando "document.getElementById("titulo").innerHTML"
+  - document : refere-se ao "body" da página HTML
+  - get : obter "Element"(Elemento)By(por)Id(identificação)
+  - inner: interno (dentro do...) HTML
+  Pegamos um elemento que está dentro do body via ID e inserimos um conteúdo em HTML, que nesse caso, é o "nome do usuário".
+*/
+document.getElementById("titulo").innerHTML = nome;
+
+/*versionar:
+git init (inicia o processo)
+git add . (coloca os arquivos)
+git commit -m "Situação" (indica um passo do processo feito)
+Versionado.
+*/
